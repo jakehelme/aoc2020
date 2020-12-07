1db82e5ad0b8890a686f0b3786f1f7bdaa659c64
Jake Helme <jake.helme@gmail.com>
2020-12-07 23:28:45 +0200

    d7p1

--- day7/part1/index.js
+++ day7/part1/index.js
@@ -1,23 +1,83 @@
-const input = require('../example.json');
+// const input = require('../example.json');
+const input = require('../input.json');
 
 (function () {
 	const collection = {};
-	input.forEach(x => {
-		console.log(x);
-		const matches = x.match(/^(\w+\s\w+)\sbags\scontain\s(\d+)\s(\w+\s\w+)\sbags?[,\.](\s(\d+)\s(\w+\s\w+)\sbags?\.)?$/);
-		const name = matches[1];
-		const bags = [];
-		bags.push({
-			qty: matches[2],
-			name: matches[3]
-		});
-		bags.push({
-			qty: matches[4],
-			name: matches[5]
-		});
-		const bag = new Bag(name, bags);
-		collection[name] = bag;
+	input.forEach(rule => {
+		// const matches = rule.match(/^(\w+\s\w+)\sbags\scontain\s((\d+)\s(\w+\s\w+)\sbags?[,\.](\s(\d+)\s(\w+\s\w+)\sbags?\.)?|no\sother\sbags\.)$/);
+		// if(!matches) {
+		// 	console.log('broken');
+		// }
+		// const name = matches[1];
+		// let bag;
+		// if(matches[2] === 'no other bags.') {
+		// 	bag = new Bag(name, []);
+		// } else {
+		// 	const bags = [];
+		// 	bags.push({
+		// 		qty: matches[3],
+		// 		name: matches[4]
+		// 	});
+		// 	if(matches[5]) {
+		// 		bags.push({
+		// 			qty: matches[6],
+		// 			name: matches[7]
+		// 		});
+		// 	}
+		// 	bag = new Bag(name, bags);
+		// }
+		if(/,/.test(rule)) {
+			const bagsRaw = rule.replace(/\w+\s\w+\sbags\scontain\s/,'').split(', ');
+			const name = rule.match(/^\w+\s\w+/)[0];
+			const bags = [];
+
+			for(let bag of bagsRaw) {
+				const matches = bag.match(/^(\d+)\s(\w+\s\w+)/);
+				bags.push({
+					qty: matches[1],
+					name: matches[2]
+				})
+			}
+			collection[name] = new Bag(name,bags);
+		} else if(/\d/.test(rule)) {
+			const bagRaw = rule.replace(/\w+\s\w+\sbags\scontain\s/,'')
+			const name = rule.match(/^\w+\s\w+/)[0];
+			const bags = [];
+			const matches = bagRaw.match(/^(\d+)\s(\w+\s\w+)/);
+			bags.push({
+				qty: matches[1],
+				name: matches[2]
+			});
+			collection[name] = new Bag(name,bags);
+		} else {
+			const name = rule.match(/^\w+\s\w+/)[0];
+			const bags = [];
+			collection[name] = new Bag(name,bags);
+		}
 	});
+
+	// console.log(collection);
+	let canContainEventually = 0;
+	for(let rule in collection) {
+		if(canContainShinyGold(collection[rule].bags)) {
+			canContainEventually++;
+		}
+	}
+	console.log(canContainEventually);
+
+	function canContainShinyGold(bags) {
+
+		for(let bag of bags) {
+			if(bag.name === 'shiny gold') {
+				return true;
+			}
+			const can = canContainShinyGold(collection[bag.name].bags);
+			if (can) {
+				return true;
+			}
+		}
+		return false;
+	}
 })();
 
 function Bag(name, bags) {
