cb79b203719f855a8d76fc64cf8bc6d28545043e
Jake Helme <jake.helme@gmail.com>
2020-12-19 11:34:10 +0200

    d18p1

---
+++ day18/part1/index.js
@@ -0,0 +1,58 @@
+// import input from '../example.js';
+import input from '../input.js';
+
+(function () {
+	const answers = [];
+	for (let exp of input) {
+		const stripped = exp.replace(/\s/g, '');
+		const arr = stripped.split('');
+		const rP = toReversePolish(arr);
+		const answer = evaluateExpression(rP);
+		answers.push(answer);
+	}
+
+	console.log(answers.reduce((p,c) => p + c));
+
+	function evaluateExpression(rp) {
+		const stack = [];
+		for(let token of rp) {
+			if(/\d/.test(token)) {
+				stack.push(token);
+			} else if(/(?:\+|\*)/.test(token)) {
+				const op1 = stack.pop();
+				const op2 = stack.pop();
+				stack.push(eval(`${op1}${token}${op2}`));
+			}
+		}
+		return stack[0];
+	}
+
+	function toReversePolish(infix) {
+		const stack = [];
+		const queue = [];
+		for(let token of infix) {
+			if(/\d/.test(token)) queue.push(token);
+			else if (/(?:\+|\*)/.test(token)) {
+				let topOfStack = stack.pop();
+				while(/(?:\+|\*)/.test(topOfStack)) {
+					queue.push(topOfStack);
+					topOfStack = stack.pop();
+				}
+				if(topOfStack) stack.push(topOfStack);
+				stack.push(token);
+			} else if(/\(/.test(token)) {
+				stack.push(token);
+			} else if(/\)/.test(token)) {
+				let topOfStack = stack.pop();
+				while(topOfStack !== '(') {
+					queue.push(topOfStack);
+					topOfStack = stack.pop();
+				}
+			}
+		}
+		for(let op of stack) {
+			queue.push(op);
+		}
+		return queue;
+	}
+})();
