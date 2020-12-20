93bb2e81c2d89188d1d2cf4b94ec6ba6f36139eb
Jake Helme <jake.helme@gmail.com>
2020-12-20 09:29:05 +0200

    d19p2

--- day19/part2/index.js
+++ day19/part2/index.js
@@ -8,19 +8,23 @@ import input from '../input.js';
 		const key = rawRule.match(/^\d+/)[0];
 		rules[key] = rawRule.replace(/\d+\:\s/, '').replace(/"/g, '').split(' ');
 	}
+	rules['8'] = ['42', '|', '42', '8'];
+  rules['11'] = ['42', '31', '|', '42', '11', '31'];
 
 	let reg = new RegExp('^' + buildRegex(rules, '0') + '$')
 
-	function buildRegex(ruleMap, key) {
+	function buildRegex(ruleMap, key, depth = 20) {
+		
 		const rule = ruleMap[key];
 
+		if(depth <= 0) return '';
 		if (rule[0] === 'a' || rule[0] === 'b') return rule[0];
 		let reg = '(';
 		for(let x of rule) {
 			if(x === '|') {
 				reg += x;
 			} else {
-				reg += buildRegex(ruleMap, x);
+				reg += buildRegex(ruleMap, x, depth - 1);
 			}
 		}
 		return `${reg})`;
